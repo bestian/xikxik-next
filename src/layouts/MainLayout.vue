bee42ec12ccd9063488c2d9d9268ecd73367cc4e
Bestian Tang 大巴 <bestian@gmail.com>
2023-12-21 20:25:04 +0800

    init

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -1,102 +1,499 @@
-<template>
-  <q-layout view="lHh Lpr lFf">
-    <q-header elevated>
-      <q-toolbar>
-        <q-btn
-          flat
-          dense
-          round
-          icon="menu"
-          aria-label="Menu"
-          @click="toggleLeftDrawer"
-        />
-
-        <q-toolbar-title>
-          Quasar App
-        </q-toolbar-title>
-
-        <div>Quasar v{{ $q.version }}</div>
-      </q-toolbar>
-    </q-header>
-
-    <q-drawer
-      v-model="leftDrawerOpen"
-      show-if-above
-      bordered
-    >
-      <q-list>
-        <q-item-label
-          header
-        >
-          Essential Links
-        </q-item-label>
-
-        <EssentialLink
-          v-for="link in essentialLinks"
-          :key="link.title"
-          v-bind="link"
-        />
-      </q-list>
-    </q-drawer>
-
-    <q-page-container>
-      <router-view />
-    </q-page-container>
-  </q-layout>
+<template lang="pug">
+q-layout(view="lHh Lpr lFf")
+  q-header(elevated)
+    q-toolbar
+      q-btn(flat, dense, round, icon="menu", aria-label="Menu", @click="toggleLeftDrawer")
+      q-toolbar-title Quasar 小道一報
+      div v2.0.0
+  q-drawer(v-model="leftDrawerOpen", show-if-above, bordered)
+    q-list
+      q-item-label(header) Essential Links
+  q-page-container
+  router-view(:si="si",
+  :xikxik = "getXikxik()",
+  newsList = "getNewsList()",
+  :poet = "getPoet()",
+  @add="add", @del = "del", @reset = "reset", @addNews="addNews", @delNews = "delNews", @resetNews = "resetNews", @addPoet="addPoet", @delPoet = "delPoet", @resetPoet = "resetPoet", @reseter="reseter")
+  ad-be
 </template>
 
-<script setup lang="ts">
-import { ref } from 'vue';
-import EssentialLink, { EssentialLinkProps } from 'components/EssentialLink.vue';
+<script>
+import { ref, onMounted, defineComponent } from 'vue';
+import Ad from '../components/Ad-Be.vue';
+import { useRouter } from 'vue-router';
 
-const essentialLinks: EssentialLinkProps[] = [
-  {
-    title: 'Docs',
-    caption: 'quasar.dev',
-    icon: 'school',
-    link: 'https://quasar.dev'
+export default defineComponent({
+  components: {
+    Ad,
   },
-  {
-    title: 'Github',
-    caption: 'github.com/quasarframework',
-    icon: 'code',
-    link: 'https://github.com/quasarframework'
-  },
-  {
-    title: 'Discord Chat Channel',
-    caption: 'chat.quasar.dev',
-    icon: 'chat',
-    link: 'https://chat.quasar.dev'
-  },
-  {
-    title: 'Forum',
-    caption: 'forum.quasar.dev',
-    icon: 'record_voice_over',
-    link: 'https://forum.quasar.dev'
-  },
-  {
-    title: 'Twitter',
-    caption: '@quasarframework',
-    icon: 'rss_feed',
-    link: 'https://twitter.quasar.dev'
-  },
-  {
-    title: 'Facebook',
-    caption: '@QuasarFramework',
-    icon: 'public',
-    link: 'https://facebook.quasar.dev'
-  },
-  {
-    title: 'Quasar Awesome',
-    caption: 'Community Quasar projects',
-    icon: 'favorite',
-    link: 'https://awesome.quasar.dev'
-  }
-];
 
-const leftDrawerOpen = ref(false)
+  setup() {
+    const router = useRouter();
+    const leftDrawerOpen = ref(false);
+    const toggleLeftDrawer = () => {
+      leftDrawerOpen.value = !leftDrawerOpen.value;
+    };
+    const si = ref(0);
+    const add = () => {
+      si.value++;
+    };
+    const del = () => {
+      si.value--;
+    };
+    const reset = () => {
+      si.value = 0;
+    };
+    const addNews = (n) => {
+      $localStorage.newsList.push(n);
+    };
+    const delNews = (n) => {
+      $localStorage.newsList.splice(n, 1);
+    };
+    const resetNews = () => {
+      $localStorage.newsList = [];
+    };
+    const addPoet = (p) => {
+      $localStorage.poet.push(p);
+    };
+    const delPoet = (p) => {
+      $localStorage.poet.splice(p, 1);
+    };
+    const resetPoet = () => {
+      $localStorage.poet = [];
+    };
+    const reseter = () => {
+      reset();
+      resetNews();
+      resetPoet();
+    };
+    return {
+      getNewsList() {
+        var news = [
+          '昨日per在place佈道，他說light',
+          '昨日per被指控在place放置adjobject，他鄭重否認。告密者per表示，他已有充份證據，將於num日後公諸於世。',
+          '昨日per宣稱，他和per在遺傳上是接近的。對方表示，這種說法純粹是為了要繼承adjobject。',
+          '最近避不露面的per終於出現了，他對外表示，連續num日他都在placevi',
+          '昨天per和per在place旁彼此vt。他們相互vt了num分鐘，最後決定一起vi',
+          'per和per昨天竟然在place旁violenceVI了num分鐘，大家都覺得很神奇',
+          'per昨天在place旁vtanimal，引來num人關注',
+        ];
+        return localStorage.getItem('newsList') || news;
+      },
+      getPoet() {
+        var poetry = [
+          'light',
+          'perpeaceVTnatureO',
+          'perpeaceVTnatureO',
+          'animalvi、vi',
+          'light',
+          'natureSvtnatureO',
+          'natureSvtnatureO',
+          'animalpeaceVI、peaceVI',
+          'light',
+        ];
+        return localStorage.getItem('poet') || poetry;
+      },
+      getXikxik() {
+        var dat = {
+          light: [
+            '任何事情發生時都不需要害怕',
+            '只要是完整的，就沒有什麼可以被剝奪。',
+            '神聖的秩序是無所不能。',
+            '孩子，你要無畏無懼。',
+            '源頭是很大的生命。',
+            '秩序就是愛',
+          ],
+          animal: [
+            '海鷗',
+            '小貓',
+            '小狗',
+            '鯨魚',
+            '水獺',
+            '烏龜',
+            '狼',
+            '獾',
+            '地鼠',
+            '蝸牛',
+            '蝙蝠',
+            '大象',
+            '獅子',
+          ],
+          noun: [
+            '天空',
+            '大火',
+            '風',
+            '雷電',
+            '浮雲',
+            '山嵐',
+            '大海',
+            '石頭',
+            '漩渦',
+            '樹',
+            '草',
+            '花',
+            '磚牆',
+            '竹籬',
+            '碗',
+            '陶窯',
+            '泥土路',
+            '紙船',
+            '風箏',
+            '馬車',
+            '柏油路',
+            '電線竿',
+            '怪手',
+            '磚牆',
+            '竹籬',
+            '碗',
+            '紙船',
+            '風箏',
+            '馬車',
+          ],
+          natureS: [
+            '天',
+            '雲',
+            '海',
+            '山',
+            '月亮',
+            '風',
+            '大雨',
+            '太陽',
+            '雷',
+            '閃電',
+            '雲豹',
+            '老虎',
+            '黑熊',
+            '山豬',
+            '飛鼠',
+            '巫師',
+            '神仙',
+          ],
+          place: [
+            '湖泊',
+            '森林',
+            '山脈',
+            '海島',
+            '平原',
+            '沼澤',
+            '高山',
+            '大地',
+            '河流',
+            '茅坑',
+            '木屋',
+            '稻田',
+            '五角大廈',
+            '金時堂',
+            '迪士尼樂園',
+            '恐怖谷',
+            '鬼屋',
+            '武道館',
+            '下水道',
+            '火車站',
+            '雲端',
+            '地獄',
+            '天堂',
+            '高速公路',
+            '外太空',
+            '星空下',
+            '美好的夏夜',
+            '午夜裡',
+            '光天化日下',
+            '大霧中',
+            '火堆旁',
+          ],
+          vi: [
+            '站著',
+            '坐著',
+            '躺著',
+            '沉默',
+            '停止',
+            '持續轉動',
+            '睡著',
+            '獨自漫歩',
+            '靜坐',
+            '自爆',
+            '休息',
+            '自爆',
+            '狂吃拉麵',
+            '原地轉圈',
+            '表演單腳跳',
+            '寫程式',
+            '祈禱',
+            '靜坐',
+            '吹泡泡',
+            '玩耍',
+            '跳舞',
+            '做實驗',
+            '算數學',
+          ],
+          peaceVI: ['等待', '沉默', '凝結', '睡去', '微笑', '祈禱'],
+          violenceVI: ['崩潰', '倒下', '碎裂', '死掉', '破掉', '消失無蹤'],
+          vt: [
+            '毆打',
+            '拍打',
+            '載運',
+            '推動',
+            '舉起',
+            '移動',
+            '衝擊',
+            '包容',
+            '擁抱',
+            '輕撫',
+            '親吻',
+            '照顧',
+            '安慰',
+            '治癒',
+            '喚醒',
+            '擁抱',
+            '扶起',
+            '牽起',
+            '保護',
+            '溫暖',
+            '呵護',
+            '安撫',
+            '深愛著',
+            '固定',
+            '敲擊',
+            '抓緊',
+            '踐踏',
+            '踢',
+            '撕裂',
+            '壓倒',
+            '撞破',
+            '衝向',
+            '搥打',
+            '揉躪',
+            '敲擊',
+            '搖晃',
+            '扭轉',
+            '壓扁',
+            '攻擊',
+            '摔破',
+          ],
+          peaceVT: [
+            '輕撫',
+            '親吻',
+            '照顧',
+            '安慰',
+            '治癒',
+            '喚醒',
+            '擁抱',
+            '扶起',
+            '牽起',
+            '保護',
+            '溫暖',
+            '呵護',
+            '安撫',
+            '深愛著',
+          ],
+          violenceVT: [
+            '拍打',
+            '踐踏',
+            '踢',
+            '撕裂',
+            '壓倒',
+            '撞破',
+            '衝向',
+            '搥打',
+            '揉躪',
+            '敲擊',
+            '搖晃',
+            '扭轉',
+            '壓扁',
+            '攻擊',
+            '摔破',
+          ],
+          blah: [
+            '沒有人知道為什麼',
+            '原來',
+            '過了很久',
+            '時光飛逝',
+            '風吹過之後',
+            '這就是，世界的夢囈',
+            '於是，我們也生生不息',
+            '無常，還是無常',
+            '喔喔喔',
+            '妙哉，善哉',
+          ],
+          num: [1, 2, 3, 5, 10, 20, 30, 50, 100, 200, 500],
+          per: [
+            '和尚',
+            '道士',
+            '外星人',
+            '刺客',
+            '魔法師',
+            '智者',
+            '戰士',
+            '修行人',
+            '小偷',
+            '修女',
+            '超人',
+            '漁夫',
+            '農婦',
+            '小女孩',
+            '小男孩',
+            '小肥蟲',
+            '外星人',
+            '電漿生物',
+            '變形蟲',
+            '蜘蛛人',
+            '神奇寶貝',
+            '秘密客',
+            '幽靈',
+            '知名藝人',
+            '米老鼠',
+            '空空道人',
+            '假面超人',
+            '魔術師',
+            '小叮噹',
+            '武林高手',
+            '數學家',
+            '櫻木花道',
+            '宮城良田',
+            '流川楓',
+            '赤木剛憲',
+            '三井壽',
+            '小火龍',
+            '傑尼龜',
+            '皮卡丘',
+            '幫派份子',
+            '苦力怕',
+            '微生物',
+            '小太陽',
+            '恐怖份子',
+          ],
+          adj: [
+            '不起眼的',
+            '爆笑的',
+            '詭異的',
+            '愚蠢的',
+            '引人省思的',
+            '意義深遠的',
+            '巨大的',
+            '寶貴的',
+            '奇特的',
+          ],
+          object: [
+            '雞蛋',
+            '釘子',
+            '巧克力',
+            '大便',
+            '小刀',
+            '骨頭',
+            '定時炸彈',
+            '水晶球',
+            '原子筆',
+            '絲巾',
+            '符咒',
+            '電子計算機',
+            '電腦',
+            '地球儀',
+            '綿被',
+            '掃把',
+            '鋤頭',
+            '石頭',
+            '石油',
+            '玩偶',
+            '滑鼠',
+            '竹筒飯',
+            '香蕉',
+            '玫瑰花',
+          ],
+          natureO: [
+            '石頭',
+            '土地',
+            '湖',
+            '小溪',
+            '雲',
+            '沖積扇',
+            '荔枝',
+            '高麗菜',
+            '野薑花',
+            '相思樹',
+            '芒草',
+            '柳樹',
+            '毛毛蟲',
+            '蚯蚓',
+            '蟑螂',
+            '烏龜',
+            '斑馬',
+            '貓',
+            '黃牛',
+            '水牛',
+            '綿羊',
+          ],
+          civilO: [
+            '磚牆',
+            '竹籬',
+            '茅坑',
+            '木屋',
+            '稻田',
+            '碗',
+            '陶窯',
+            '泥土路',
+            '紙船',
+            '風箏',
+            '馬車',
+            '柏油路',
+            '電線竿',
+            '怪手',
+            '磚牆',
+            '竹籬',
+            '茅坑',
+            '木屋',
+            '稻田',
+            '碗',
+          ],
+        };
 
-function toggleLeftDrawer() {
-  leftDrawerOpen.value = !leftDrawerOpen.value
-}
+        return localStorage.getItem('xikxik') || dat;
+      },
+      leftDrawerOpen,
+      toggleLeftDrawer,
+      si,
+      add,
+      del,
+      reset,
+      addNews,
+      delNews,
+      resetNews,
+      addPoet,
+      delPoet,
+      resetPoet,
+      reseter,
+    };
+  },
+});
 </script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif, 'Microsoft JhengHei',
+    'Microsoft YaHei', 'STHeiti', 'Apple LiGothic Medium';
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+a,
+button {
+  cursor: pointer;
+}
+
+@media screen and (max-width: 420px) {
+  .fat-only {
+    display: none !important;
+  }
+}
+
+@media screen and (min-width: 421px) {
+  .thin-only {
+    display: none !important;
+  }
+}
+
+#main {
+  margin-top: 80px;
+}
+</style>
--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,42 +1,78 @@
-<template>
-  <q-page class="row items-center justify-evenly">
-    <example-component
-      title="Example component"
-      active
-      :todos="todos"
-      :meta="meta"
-    ></example-component>
-  </q-page>
+<template lang="pug">
+.hello
+  .ui.container.grid
+    #poet.two.column.doubling.row
+      .six.wide.column.ui.blue.segment
+        h1 拾詩氏
+        img(src="https://picsum.photos/400/200", alt="Lorem")
+        hr
+        .ui.list
+          .item(v-for = "(p, idx) in poet" v-bind:key = "p + idx")
+            h2 {{ p }}
+            // h2 {{ s(parse(p)) }}
+      .ten.wide.column.left.aligned.ui.black.segment
+        h1 小道小報 {{ today.getFullYear() + '-' + (today.getMonth() + 1) + '-' + today.getDate() }}
+        //InArticleAdsense(data-ad-client="ca-pub-7209910540592367", data-ad-slot="8130621052")
+        h2 (本報訊)
+        .ui.bulleted.celled.list
+          .item(v-for = "n in newsList" v-bind:key = "n")
+            h3 {{ n }}
+            // h3 {{ s(parse(n)) }}
+    //.one.column.row
+      .column.ui.segment
+        InArticleAdsense(data-ad-client="ca-pub-7209910540592367", data-ad-slot="8130621052")
+    .one.column.row
+      .column
+        a.ui.huge.black.button(@click = "reseter()") 再來！
+    .one.column.row
+      .column
+        .fb-comments(data-href="http://xikxik.bestian.tw" data-width="" data-numposts="5")
 </template>
 
-<script setup lang="ts">
-import { Todo, Meta } from 'components/models';
-import ExampleComponent from 'components/ExampleComponent.vue';
-import { ref } from 'vue';
+<script>
+import { defineComponent, ref } from 'vue';
 
-const todos = ref<Todo[]>([
+export default defineComponent(
   {
-    id: 1,
-    content: 'ct1'
-  },
-  {
-    id: 2,
-    content: 'ct2'
-  },
-  {
-    id: 3,
-    content: 'ct3'
-  },
-  {
-    id: 4,
-    content: 'ct4'
-  },
-  {
-    id: 5,
-    content: 'ct5'
+    setup() {
+      const today = ref(new Date());
+
+      const reseter = () => {
+        console.log('Resetter triggered');
+        this.$emit('reseter');
+      };
+
+      // 如果有其他需要的数据或方法，请在这里声明
+      // ...
+
+      return {
+        today,
+        reseter,
+        // 其他需要在模板中使用的响应式数据或方法
+      };
+    },
+
+    props: ['xikxik', 'newsList', 'poet', 'parse'],
   }
-]);
-const meta = ref<Meta>({
-  totalCount: 1200
-});
+
+  // 如果有需要的话，这里可以定义其他的生命周期钩子或者方法
+  // ...
+);
 </script>
+
+<style scoped>
+a {
+  color: #42b983;
+}
+
+.ui.grid > .blue.column,
+.ui.grid > .blue.row,
+.ui.grid > .row > .blue.column {
+  background-color: #009 !important;
+}
+
+#poet {
+  font-family: 'Microsoft JhengHei', 'DFKai-SB', 'PMingLiU', 'Heiti TC',
+    'LiHei Pro', 'BiauKai';
+}
+</style>
